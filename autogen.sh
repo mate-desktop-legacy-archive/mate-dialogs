001547a4b04c531a866f2df18478fa9c6a9dd4ea
Steve Zesch <stevezesch2@gmail.com>
2012-02-22 21:06:47 -0500

    Preparing for 1.2 release

--- autogen.sh
+++ autogen.sh
@@ -14,11 +14,11 @@ REQUIRED_AUTOMAKE_VERSION=1.9
 }
 
 
-which mate-autogen.sh || {
+which mate-autogen || {
     echo "You need to install mate-common from the MATE CVS"
     exit 1
 }
 
 USE_MATE2_MACROS=1
 
-. mate-autogen.sh
+. mate-autogen
